118623d428e046fee17b18e582c1eedc14117fac
haseebdivinatorysolutions <haseeb@divinatorysolutions.com>
2022-08-04 20:22:45 +0500

    First Commit by Me

---
+++ vue/src/store/index.js
@@ -0,0 +1,17 @@
+import {createStore} from "vuex"
+
+const store = createStore({
+    state:{
+        user:{
+            data:{name:'haseeb'},
+            token:null
+        }
+    },
+    getters:{},
+    actions:{},
+    mutations:{},
+    modules:{}
+})
+
+
+export default store
